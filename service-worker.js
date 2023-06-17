6b2405d1fa4834ce83eb02c50af5223839b510a0
rifat_cholakov <rifat.cholakov@pariplaydev.com>
2023-06-17 12:49:14 +0300

    Added dynamic caching and fallback page for offline mode.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,23 @@
+cacheName = 'my-cache-v1';
+dynamicCache = 'my-dynamic-cache-v1';
+
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if(keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+}
+
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('my-cache').then(cache => {
+        caches.open(cacheName).then(cache => {
             return cache.addAll([
                 '/',
                 '/index.html',
+                '/fallback.html',
                 '/style.css',
                 '/script.js',
                 '/gotse-delchev-time.json',
@@ -16,15 +30,30 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener("activate", evt => {
-    console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== cacheName && key !== dynamicCache)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        }).catch(error => {
-            console.error('Error in fetch handler:', error);
+            return response || fetch(event.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    limitCacheSize(dynamicCache, 50);
+                    return fetchRes;
+                });
+            });
+        }).catch(() => {
+            if (evt.request.indexOf('.html') > -1) {
+                return caches.match('/fallback.html')
+            }
         })
     );
 });
